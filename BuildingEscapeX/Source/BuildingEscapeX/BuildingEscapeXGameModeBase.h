e716297ff29004babac36de9ef5967de77c4f912
Yash Kulthe <yashkulthex@gmail.com>
2020-01-25 20:00:52 +0530

    Starting with the project

---
+++ BuildingEscapeX/Source/BuildingEscapeX/BuildingEscapeXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEX_API ABuildingEscapeXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
