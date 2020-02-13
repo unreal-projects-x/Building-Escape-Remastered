59198d90a814f76d9273874a4b3baa4d99f212c0
Yash Kulthe <yashkulthex@gmail.com>
2020-02-13 22:01:12 +0530

    Rotation
    rotating the door using c++

---
+++ BuildingEscapeX/Source/BuildingEscapeX/OpenDoor.h
@@ -0,0 +1,30 @@
+// Copyright Yash Kulthe
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPEX_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+private:
+	FRotator NewRotation;		
+};
